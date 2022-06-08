73c29f9a232941e67a2f2d56a96892763f48948e
Guigaspaco <yoshiro2@gmail.com>
2022-06-07 21:36:16 -0300

    Teste

---
+++ src/test/java/com/guigasprojeto1/newuser/NewuserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guigasprojeto1.newuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewuserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
